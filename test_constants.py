4cb1318b976bb0f876a74153cb3a96763da0594f
Pearu Pung <pungpearu@gmail.com>
2022-09-25 22:56:20 +0300

    #13 Test listing playlists and finding Youtube links

---
+++ test_constants.py
@@ -0,0 +1,256 @@
+playlist_json = """[
+{
+  "name": "Liked Songs",
+  "tracks": [
+    {
+      "added_at": "2019-06-02T11:22:29Z",
+      "track": {
+        "album": {
+          "album_type": "album",
+          "artists": [
+            {
+              "external_urls": {
+                "spotify": "https://open.spotify.com/artist/7c9O0hfRy2u32JVcWhoope"
+              },
+              "href": "https://api.spotify.com/v1/artists/7c9O0hfRy2u32JVcWhoope",
+              "id": "7c9O0hfRy2u32JVcWhoope",
+              "name": "Lars Danielsson",
+              "type": "artist",
+              "uri": "spotify:artist:7c9O0hfRy2u32JVcWhoope"
+            }
+          ],
+          "external_urls": {
+            "spotify": "https://open.spotify.com/album/4JrArgXTBvpBwfobLo0nz3"
+          },
+          "href": "https://api.spotify.com/v1/albums/4JrArgXTBvpBwfobLo0nz3",
+          "id": "4JrArgXTBvpBwfobLo0nz3",
+          "images": [
+            {
+              "height": 640,
+              "url": "https://i.scdn.co/image/ab67616d0000b27313439cddec8c538356e21410",
+              "width": 640
+            },
+            {
+              "height": 300,
+              "url": "https://i.scdn.co/image/ab67616d00001e0213439cddec8c538356e21410",
+              "width": 300
+            },
+            {
+              "height": 64,
+              "url": "https://i.scdn.co/image/ab67616d0000485113439cddec8c538356e21410",
+              "width": 64
+            }
+          ],
+          "name": "Liberetto III (with Grégory Privat, John Parricelli & Magnus Öström)",
+          "release_date": "2017-05-26",
+          "release_date_precision": "day",
+          "total_tracks": 12,
+          "type": "album",
+          "uri": "spotify:album:4JrArgXTBvpBwfobLo0nz3"
+        },
+        "artists": [
+          {
+            "external_urls": {
+              "spotify": "https://open.spotify.com/artist/7c9O0hfRy2u32JVcWhoope"
+            },
+            "href": "https://api.spotify.com/v1/artists/7c9O0hfRy2u32JVcWhoope",
+            "id": "7c9O0hfRy2u32JVcWhoope",
+            "name": "Lars Danielsson",
+            "type": "artist",
+            "uri": "spotify:artist:7c9O0hfRy2u32JVcWhoope"
+          },
+          {
+            "external_urls": {
+              "spotify": "https://open.spotify.com/artist/0mmRcGkPj9s4Hq0xFrMBPh"
+            },
+            "href": "https://api.spotify.com/v1/artists/0mmRcGkPj9s4Hq0xFrMBPh",
+            "id": "0mmRcGkPj9s4Hq0xFrMBPh",
+            "name": "Grégory Privat",
+            "type": "artist",
+            "uri": "spotify:artist:0mmRcGkPj9s4Hq0xFrMBPh"
+          },
+          {
+            "external_urls": {
+              "spotify": "https://open.spotify.com/artist/55Ib86Ob0dKLELhJ1gvUri"
+            },
+            "href": "https://api.spotify.com/v1/artists/55Ib86Ob0dKLELhJ1gvUri",
+            "id": "55Ib86Ob0dKLELhJ1gvUri",
+            "name": "John Parricelli",
+            "type": "artist",
+            "uri": "spotify:artist:55Ib86Ob0dKLELhJ1gvUri"
+          },
+          {
+            "external_urls": {
+              "spotify": "https://open.spotify.com/artist/1ZPtjLlwSZjToQXcq2JVy2"
+            },
+            "href": "https://api.spotify.com/v1/artists/1ZPtjLlwSZjToQXcq2JVy2",
+            "id": "1ZPtjLlwSZjToQXcq2JVy2",
+            "name": "Magnus Öström",
+            "type": "artist",
+            "uri": "spotify:artist:1ZPtjLlwSZjToQXcq2JVy2"
+          }
+        ],
+        "disc_number": 1,
+        "duration_ms": 277853,
+        "explicit": false,
+        "external_ids": {
+          "isrc": "DEA891700257"
+        },
+        "external_urls": {
+          "spotify": "https://open.spotify.com/track/44ztnLZZhnYbmjDNeZOhL2"
+        },
+        "href": "https://api.spotify.com/v1/tracks/44ztnLZZhnYbmjDNeZOhL2",
+        "id": "44ztnLZZhnYbmjDNeZOhL2",
+        "is_local": false,
+        "name": "Lviv",
+        "popularity": 28,
+        "preview_url": "https://p.scdn.co/mp3-preview/d85d9c0cac44a9431ff1b711c276b75391abd760?cid=5c098bcc800e45d49e476265bc9b6934",
+        "track_number": 3,
+        "type": "track",
+        "uri": "spotify:track:44ztnLZZhnYbmjDNeZOhL2"
+      }
+    }
+  ]
+},
+{
+  "collaborative": false,
+  "description": "",
+  "external_urls": {
+    "spotify": "https://open.spotify.com/playlist/6nE1Nd8MOlDlNLJUUNTuSl"
+  },
+  "href": "https://api.spotify.com/v1/playlists/6nE1Nd8MOlDlNLJUUNTuSl",
+  "id": "6nE1Nd8MOlDlNLJUUNTuSl",
+  "images": [
+    {
+      "height": 640,
+      "url": "https://i.scdn.co/image/ab67616d0000b273e37608ac9405cd4163c20207",
+      "width": 640
+    }
+  ],
+  "name": "testing",
+  "owner": {
+    "display_name": "Pearu Pung",
+    "external_urls": {
+      "spotify": "https://open.spotify.com/user/pungpearu"
+    },
+    "href": "https://api.spotify.com/v1/users/pungpearu",
+    "id": "pungpearu",
+    "type": "user",
+    "uri": "spotify:user:pungpearu"
+  },
+  "primary_color": null,
+  "public": true,
+  "snapshot_id": "Miw1YmQ5ZjZiNzZmOTRhMWI1MDMxNjA4MTY5NTljYjRmMDhhNzkwYWNi",
+  "tracks": [
+    {
+      "added_at": "2022-09-25T18:24:59Z",
+      "added_by": {
+        "external_urls": {
+          "spotify": "https://open.spotify.com/user/pungpearu"
+        },
+        "href": "https://api.spotify.com/v1/users/pungpearu",
+        "id": "pungpearu",
+        "type": "user",
+        "uri": "spotify:user:pungpearu"
+      },
+      "is_local": false,
+      "primary_color": null,
+      "track": {
+        "album": {
+          "album_type": "album",
+          "artists": [
+            {
+              "external_urls": {
+                "spotify": "https://open.spotify.com/artist/041iTeoMIwXMlShuQPIVKo"
+              },
+              "href": "https://api.spotify.com/v1/artists/041iTeoMIwXMlShuQPIVKo",
+              "id": "041iTeoMIwXMlShuQPIVKo",
+              "name": "Klangkarussell",
+              "type": "artist",
+              "uri": "spotify:artist:041iTeoMIwXMlShuQPIVKo"
+            }
+          ],
+          "external_urls": {
+            "spotify": "https://open.spotify.com/album/0CoiuzFtZVRk9imDnfate2"
+          },
+          "href": "https://api.spotify.com/v1/albums/0CoiuzFtZVRk9imDnfate2",
+          "id": "0CoiuzFtZVRk9imDnfate2",
+          "images": [
+            {
+              "height": 640,
+              "url": "https://i.scdn.co/image/ab67616d0000b273e37608ac9405cd4163c20207",
+              "width": 640
+            },
+            {
+              "height": 300,
+              "url": "https://i.scdn.co/image/ab67616d00001e02e37608ac9405cd4163c20207",
+              "width": 300
+            },
+            {
+              "height": 64,
+              "url": "https://i.scdn.co/image/ab67616d00004851e37608ac9405cd4163c20207",
+              "width": 64
+            }
+          ],
+          "name": "Netzwerk",
+          "release_date": "2014-01-01",
+          "release_date_precision": "day",
+          "total_tracks": 11,
+          "type": "album",
+          "uri": "spotify:album:0CoiuzFtZVRk9imDnfate2"
+        },
+        "artists": [
+          {
+            "external_urls": {
+              "spotify": "https://open.spotify.com/artist/041iTeoMIwXMlShuQPIVKo"
+            },
+            "href": "https://api.spotify.com/v1/artists/041iTeoMIwXMlShuQPIVKo",
+            "id": "041iTeoMIwXMlShuQPIVKo",
+            "name": "Klangkarussell",
+            "type": "artist",
+            "uri": "spotify:artist:041iTeoMIwXMlShuQPIVKo"
+          },
+          {
+            "external_urls": {
+              "spotify": "https://open.spotify.com/artist/39AZSw4A8hCFWunEg2k89Z"
+            },
+            "href": "https://api.spotify.com/v1/artists/39AZSw4A8hCFWunEg2k89Z",
+            "id": "39AZSw4A8hCFWunEg2k89Z",
+            "name": "Will Heard",
+            "type": "artist",
+            "uri": "spotify:artist:39AZSw4A8hCFWunEg2k89Z"
+          }
+        ],
+        "disc_number": 1,
+        "duration_ms": 238120,
+        "episode": false,
+        "explicit": false,
+        "external_ids": {
+          "isrc": "DEUM71302894"
+        },
+        "external_urls": {
+          "spotify": "https://open.spotify.com/track/6Qi0Wls2EaolwPMPMxfe5f"
+        },
+        "href": "https://api.spotify.com/v1/tracks/6Qi0Wls2EaolwPMPMxfe5f",
+        "id": "6Qi0Wls2EaolwPMPMxfe5f",
+        "is_local": false,
+        "name": "Sonnentanz - Sun Don't Shine",
+        "popularity": 69,
+        "preview_url": null,
+        "track": true,
+        "track_number": 11,
+        "type": "track",
+        "uri": "spotify:track:6Qi0Wls2EaolwPMPMxfe5f"
+      },
+      "video_thumbnail": {
+        "url": null
+      }
+    }
+  ],
+  "type": "playlist",
+  "uri": "spotify:playlist:6nE1Nd8MOlDlNLJUUNTuSl"
+}
+]
+"""
+isrc_to_data_liked_songs_active = """{"spotify_token": null, "isrc_to_access_url": {}, "sus_tracks": {}, "is_autogen": {}, "manual_confirm": {}, "isrc_to_track_data": {}, "metadata_version": {}, "ignored_tracks": {}, "active_playlist_ids": {"0": true}, "isrc_local_downloaded_status": {}, "playlist_compositions": {}}"""
+ytfy_data_liked_songs_active = r"""{"spotify_token": null, "isrc_to_access_url": {"DEA891700257": "https://youtube.com/watch?v=r5wyaauKkjg"}, "sus_tracks": {}, "is_autogen": {}, "manual_confirm": {}, "isrc_to_track_data": {"DEA891700257": {"artists": ["Lars Danielsson", "Gr\u00e9gory Privat", "John Parricelli", "Magnus \u00d6str\u00f6m"], "title": "Lviv", "filename": "Lars Danielsson - Lviv"}}, "metadata_version": {}, "ignored_tracks": {}, "active_playlist_ids": {"0": true}, "isrc_local_downloaded_status": {}, "playlist_compositions": {}}"""
