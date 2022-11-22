cb6fab4e1cfa3a62a57d06f1302605a4cc073148
Henri Laiho <henriharri.laiho@gmail.com>
2022-11-23 00:06:55 +0200

    Set fast_youtubify.ps1 executable

--- fast_youtubify.ps1
+++ fast_youtubify.ps1
@@ -1,4 +1,4 @@
-pip install --upgrade -r requirements.txt
+python -m pip install --upgrade -r requirements.txt
 python spotify_import.py --liked_fuzzy
 python sync_with_others.py
 python youtubify.py convert
